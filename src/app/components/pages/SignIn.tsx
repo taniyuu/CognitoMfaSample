6f1b6f9d81bf1f5160d889dcd3832305296b621d
taniyuu <cmt.0801nonki@gmail.com>
2020-10-01 18:03:20 +0900

    ダサいけど、せりあがるようにはなった

--- src/app/components/pages/SignIn.tsx
+++ src/app/components/pages/SignIn.tsx
@@ -2,39 +2,51 @@ import React from "react";
 import {
   View,
   Text,
-  StyleSheet,
+  KeyboardAvoidingView,
   TextInput,
   TouchableWithoutFeedback,
   Keyboard,
+  Platform,
+  StyleSheet,
 } from "react-native";
 
 const SignIn = () => {
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-        <TextInput
-          style={styles.formControl}
-          keyboardType="default"
-          value=""
-          placeholder="ユーザ名"
-          returnKeyType="done"
-        />
-        <TextInput
-          style={styles.formControl}
-          keyboardType="ascii-capable"
-          value=""
-          placeholder="パスワード"
-          returnKeyType="done"
-        />
-      </View>
-    </TouchableWithoutFeedback>
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <Text>Home Screen</Text>
+          <TextInput
+            style={styles.formControl}
+            keyboardType="default"
+            value=""
+            placeholder="ユーザ名"
+            returnKeyType="done"
+          />
+          <TextInput
+            style={styles.formControl}
+            keyboardType="ascii-capable"
+            value=""
+            placeholder="パスワード"
+            returnKeyType="done"
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
 export default SignIn;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   formControl: {
     height: 40,
     width: 160,
